819ff24c88b8c268206d797a228351035a0d85e6
unknown <aurangzebhusain786@gmail.com>
2020-09-19 21:46:35 +0530

    testing json file

---
+++ precache-manifest.1968762b85160ca7ceb236abb3896122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bf93ec3021767264d591e6f4575fec",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "a09ab50b9b02af1971f8",
+    "url": "/cv/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/cv/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/cv/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09ab50b9b02af1971f8",
+    "url": "/cv/static/js/main.44abe933.chunk.js"
+  },
+  {
+    "revision": "81810ce1387fc8a711cb",
+    "url": "/cv/static/js/runtime-main.3807942a.js"
+  }
+]);
