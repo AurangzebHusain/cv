8484f4aec9d56d4aa700caed9bd8fb2b82801d8b
unknown <aurangzebhusain786@gmail.com>
2020-09-19 21:32:27 +0530

    Updates

---
+++ precache-manifest.221e7c6bc3e261ea6e8db11730733ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86495062194b215a57114b1ee886174",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "c374be9a6c37bf086d93",
+    "url": "/cv/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/cv/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/cv/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c374be9a6c37bf086d93",
+    "url": "/cv/static/js/main.b0c4115f.chunk.js"
+  },
+  {
+    "revision": "81810ce1387fc8a711cb",
+    "url": "/cv/static/js/runtime-main.3807942a.js"
+  }
+]);
